3b3f6739e4baa7294b40c6810f833af7d0cfd781
Rafael <rafa.shiga@gmail.com>
2022-06-22 15:40:39 -0300

    feat: add style

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,22 @@
+import { CheckCircle } from 'phosphor-react';
+
 export function Lesson() {
-	return <div>Lesson</div>;
+	return (
+		<a href='#'>
+			<span className='text-gray-300'>Terça • 22 de junho • 19h00</span>
+			<div className='rounded border border-gray-500 p-4 mt-2'>
+				<header className='flex items-center justify-between'>
+					<span className='flex items-center gap-2 text-sm text-blue-500 font-medium'>
+						<CheckCircle size={20} /> Conteúdo Liberado
+					</span>
+					<span className='text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold'>
+						AO VIVO
+					</span>
+				</header>
+				<strong className='text-gray-200 mt-5 block'>
+					Abertura do evento Ignite lab
+				</strong>
+			</div>
+		</a>
+	);
 }
